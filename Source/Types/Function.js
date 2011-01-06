38ef3f3ab8941eb25aaa90d750230d02a5f52f0f
Shock <shock@shock.(none)>
2011-01-07 00:27:58 +0200

    Refactoring: new structure, preparing for Packager. A lot of new features: Class,Types,Core, etc

---
+++ Source/Types/Function.js
@@ -0,0 +1,60 @@
+/*
+---
+
+name: Array
+
+description: Contains Function Prototypes like context, periodical and delay.
+
+license: [GNU Lesser General Public License](http://opensource.org/licenses/lgpl-license.php)
+
+requires: atom
+
+provides: Function
+
+...
+*/
+
+new function () {
+	atom.extend(Function, 'safe', {
+		lambda : function (value) {
+			var returnThis = (arguments.length == 0);
+			return function () { return returnThis ? this : value; };
+		},
+		log: function (msg) {
+			var args = arguments.length ? arguments : null;
+			return function () {
+				atom.log.apply(atom, args || [this]);
+			};
+		}
+	});
+
+	atom.implement(Function, 'safe', {
+		context: function(bind, args){
+			var fn = this;
+			args = args ? atom.toArray(args) : [];
+			return function(){
+				return fn.apply(bind === false ? this : bind, args.append(arguments));
+			};
+		}
+	});
+
+	var timeout = {
+		set : {
+			Timeout : setTimeout,
+			Interval: setInterval
+		},
+		clear : {
+			Timeout : function () { clearTimeout (this); },
+			Interval: function () { clearInterval(this); }
+		},
+		run: function (name, time, bind, args) {
+			var result  = timeout.set[name].call(null, this.context(bind, args), time);
+			result.stop = timeout.clear[name].context(result);
+			return result;
+		}
+	};
+	atom.implement(Function, 'safe', {
+		delay:      timeout.run.context(false, ['Timeout']),
+		periodical: timeout.run.context(false, ['Interval'])
+	});
+}(); 
---
+++ Source/Types/String.js
@@ -0,0 +1,50 @@
+/*
+---
+
+name: String
+
+description: Contains String Prototypes like repeat, substitute, replaceAll and begins.
+
+license: [GNU Lesser General Public License](http://opensource.org/licenses/lgpl-license.php)
+
+requires: atom
+
+provides: String
+
+...
+*/
+
+atom.implement(String, 'safe', {
+	safeHTML: function () {
+		return this.replace(/[<'&">]/g, function (symb) {
+			return {
+				'&'  : '&amp;',
+				'\'' : '&#039;',
+				'\"' : '&quot;',
+				'<'  : '&lt;',
+				'>'  : '&gt;'
+			}[symb];
+		});
+	},
+	repeat: function(times) {
+		return new Array(times + 1).join(this);
+	},
+	substitute: function(object, regexp){
+		return this.replace(regexp || (/\\?\{([^{}]+)\}/g), function(match, name){
+			return (match[0] == '\\') ? match.slice(1) : (object[name] || '');
+		});
+	},
+	replaceAll: function (find, replace) {
+		return this.split(find).join(replace);
+	},
+	begins: function (w, caseInsensitive) {
+		return (!caseInsensitive) ? w == this.substr(0, w.length) :
+			w.toLowerCase() == this.substr(0, w.length).toLowerCase();
+	},
+	ucfirst : function () {
+		return this[0].toUpperCase() + this.substr(1);
+	},
+	lcfirst : function () {
+		return this[0].toLowerCase() + this.substr(1);
+	}
+}); 
