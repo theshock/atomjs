a96ff95aa5329a5c8346a730a5efa2989acd9e50
Shock <shocksilien@gmail.com>
2011-01-10 06:17:22 +0200

    [AtomJS Plugin] Uri

---
+++ Source/Uri.js
@@ -0,0 +1,47 @@
+/*
+---
+
+name: "[AtomJS Plugin] Uri"
+
+description: "Port of parseUri function"
+
+license: "MIT License"
+
+author: "Steven Levithan <stevenlevithan.com>"
+
+requires: [atom]
+
+provides: [atom.uri]
+
+...
+*/
+atom.extend({
+	uri: atom.extend(function (str) {
+		var	o   = atom.uri.options,
+			m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str || window.location.href),
+			uri = {},
+			i   = 14;
+
+		while (i--) uri[o.key[i]] = m[i] || "";
+
+		uri[o.q.name] = {};
+		uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+			if ($1) uri[o.q.name][$1] = $2;
+		});
+
+		return uri;
+	}, {
+		options: {
+			strictMode: false,
+			key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+			q:   {
+				name:   "queryKey",
+				parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+			},
+			parser: {
+				strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+				loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+			}
+		}
+	})
+});
