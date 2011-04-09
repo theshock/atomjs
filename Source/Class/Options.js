d10cee6d4f2db13c588859321d9ae398e234be82
Shock <shocksilien@gmail.com>
2011-04-09 23:40:24 +0300

    defaultOptions

--- Source/Class/Options.js
+++ Source/Class/Options.js
@@ -19,20 +19,21 @@ provides: Class.Options
 ...
 */
 
-atom.extend(atom.Class, {
-	Options: atom.Class({
-		setOptions: function(){
-			if (!this.options) this.options = {};
-
-			var args = [{}, this.options].append(arguments);
-			var options = this.options = atom.merge.apply(null, args);
-			if (this.addEvent) for (var option in options){
-				if (atom.typeOf(options[option]) == 'function' && (/^on[A-Z]/).test(option)) {
-					this.addEvent(option, options[option]);
-					delete options[option];
-				}
-			}
-			return this;
+atom.Class.Options = atom.Class({
+	setOptions: function(){
+		if (!this.options) {
+			var def = this.self.defaultOptions;
+			this.options = def ? atom.clone(def) : {};
 		}
-	})
+
+		var args = [{}, this.options].append(arguments);
+		var options = this.options = atom.merge.apply(null, args);
+		if (this.addEvent) for (var option in options){
+			if (atom.typeOf(options[option]) == 'function' && (/^on[A-Z]/).test(option)) {
+				this.addEvent(option, options[option]);
+				delete options[option];
+			}
+		}
+		return this;
+	}
 });
