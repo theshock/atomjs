039f86f10c24240244c2dae0a527e1f063d42e46
Shock <shocksilien@gmail.com>
2011-04-19 02:08:37 +0300

    accessors

--- atom-full-compiled.js
+++ atom-full-compiled.js
@@ -182,19 +182,12 @@ provides: accessors
 */
 
 (function (Object) {
-	var getAccessors = Object.getOwnPropertyDescriptor ?
-		function (from, key, bool) {
-			var descriptor = Object.getOwnPropertyDescriptor(from, key);
-			if (descriptor && (descriptor.set || descriptor.get) ) {
-				if (bool) return true;
+	var standard = !!Object.getOwnPropertyDescriptor, nonStandard = !!{}.__defineGetter__;
 
-				return {
-					set: descriptor.set,
-					get: descriptor.get
-				};
-			}
-			return bool ? false : null;
-		} : function (from, key, bool) {
+	if (!standard && !nonStandard) throw new Error('Accessors are not supported');
+
+	var getAccessors = nonStandard ?
+		function (from, key, bool) {
 			var g = from.__lookupGetter__(key), s = from.__lookupSetter__(key);
 
 			if ( g || s ) {
@@ -205,16 +198,39 @@ provides: accessors
 				};
 			}
 			return bool ? false : null;
+		} :
+		function (from, key, bool) {
+			var descriptor = Object.getOwnPropertyDescriptor(from, key);
+			if (!descriptor) {
+				// try to find accessors according to chain of prototypes
+				var proto = Object.getPrototypeOf(from);
+				if (proto) return getAccessors(proto, key, bool);
+			}
+
+			if (descriptor && (descriptor.set || descriptor.get) ) {
+				if (bool) return true;
+
+				return {
+					set: descriptor.set,
+					get: descriptor.get
+				};
+			}
+			return bool ? false : null;
 		}; /* getAccessors */
 
 	var setAccessors = function (object, prop, descriptor) {
 		if (descriptor) {
-			if (Object.defineProperty) {
-					for (var i in descriptor) if (['set', 'get'].indexOf(i) == -1) throw new TypeError('Unknown property: ' + i);
-					Object.defineProperty(object, prop, descriptor);
-			} else {
+			if (nonStandard) {
 				if (descriptor.get) object.__defineGetter__(prop, descriptor.get);
 				if (descriptor.set) object.__defineSetter__(prop, descriptor.set);
+			} else {
+				var desc = {
+					get: descriptor.get,
+					set: descriptor.set,
+					configurable: true,
+					enumerable: true
+				};
+				Object.defineProperty(object, prop, desc);
 			}
 		}
 		return object;
@@ -225,8 +241,6 @@ provides: accessors
 	};
 
 	var inheritAccessors = function (from, to, key) {
-		if (key == null) return hasAccessors(from, /* key */ to);
-
 		var a = getAccessors(from, key);
 
 		if ( a ) {
@@ -798,23 +812,25 @@ extend(Class, {
 			retain = fn;
 		}
 
-		for (var key in params) if (!accessors(params, this[prototype], key)) {
-			var value = params[key];
+		for (var key in params) {
+			if (!accessors(params, this[prototype], key)) {
+				var value = params[key];
 
-			if (Class.Mutators.hasOwnProperty(key)){
-				value = Class.Mutators[key].call(this, value);
-				if (value == null) continue;
-			}
-
-			if (typeOf(value) == 'function'){
-				if (value.$hidden == 'next') {
-					value.$hidden = true
-				} else if (value.$hidden) {
-					continue;
+				if (Class.Mutators.hasOwnProperty(key)){
+					value = Class.Mutators[key].call(this, value);
+					if (value == null) continue;
 				}
-				this[prototype][key] = (retain) ? value : wrap(this, key, value);
-			} else {
-				this[prototype][key] = atom.clone(value);
+
+				if (typeOf(value) == 'function'){
+					if (value.$hidden == 'next') {
+						value.$hidden = true
+					} else if (value.$hidden) {
+						continue;
+					}
+					this[prototype][key] = (retain) ? value : wrap(this, key, value);
+				} else {
+					this[prototype][key] = atom.clone(value);
+				}
 			}
 		}
 		return this;
