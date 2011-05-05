8f1545e58590e123e34268b1fb92afed5d24b86b
extempl <extempl@gmail.com>
2011-05-05 09:08:58 -0700

    same as attr

--- Source/Dom.js
+++ Source/Dom.js
@@ -190,8 +190,8 @@ new function () {
 		},
 		css : function (css) {
 			css = setter(arguments);
-			if (typeof css[0] == 'string') {
-				return this.first.style[css[0]];
+			if (typeof css == 'string') {
+				return this.first.style[css];
 			}
 			return this.each(function (elem) {
 				for (var i in css) {
