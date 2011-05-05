96cafd13f1d6a0580671496241af059a82fcfc57
extempl <extempl@gmail.com>
2011-05-05 23:05:20 +0300

    fixed errors in tests

--- Tests/tests/dom.js
+++ Tests/tests/dom.js
@@ -44,18 +44,18 @@ test('atom.attr', function() {
 test('atom.css', function() {
 	var $elem = atom.dom($ID + ' code');
 
-	strictEqual($elem.css('color'), '#999', 'inline style "color" of atom.dom("#cid code") must equal to "#999"');
-	strictEqual($elem.css('position'), 'absolute', 'css style "position" of atom.dom("#cid code") must equal to "absolute"');
+	strictEqual($elem.css('color'), 'rgb(150, 150, 150)', 'inline style "color" of atom.dom("#cid code") must equal to "rgb(150, 150, 150)"');
+	strictEqual(atom.dom($ID).css('position'), 'absolute', 'css style "position" of atom.dom("#cid") must equal to "absolute"');
 
-	$elem.css('color', '#888');
-	strictEqual($elem.css('color'), '#888', 'set the css style "color" of atom.dom("#cid code") should be equal to "#888"');
+	$elem.css('color', 'rgba(100, 100, 100)');
+	strictEqual($elem.css('color'), 'rgba(100, 100, 100)', 'set the css style "color" of atom.dom("#cid code") should be equal to "rgba(100, 100, 100)"');
 
 	$elem.css({
-		color: '#777',
-		backgroundColor: '#333'
+		color: 'rgba(80, 80, 80)',
+		background—olor: 'rgba(30, 30, 30)'
 	});
-	strictEqual($elem.css('color'), '#777', 'set the array of css styles ("color") of atom.dom("#cid code") should be equal to "#777"');
-	strictEqual($elem.css('backgroundColor'), '#333', 'set the array of css styles ("backgroundColor") of atom.dom("#cid code") should be equal to "#333"');
+	strictEqual($elem.css('color'), 'rgba(80, 80, 80)', 'set the css style ("color") of atom.dom("#cid code") should be equal to "rgba(80, 80, 80)"');
+	strictEqual($elem.css('background-color'), 'rgba(30, 30, 30)', 'set the css style ("backgroundColor") of atom.dom("#cid code") should be equal to "rgba(30, 30, 30)"');
 });
 
 };
