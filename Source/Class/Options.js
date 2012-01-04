ebd8e7678f11bf14b27f7a3a8907fbd70e02ca8d
TheShock <shocksilien@gmail.com>
2012-01-04 16:54:21 +0400

    fastSetOptions

--- Source/Class/Options.js
+++ Source/Class/Options.js
@@ -23,6 +23,7 @@ provides: Class.Options
 
 atom.Class.Options = atom.Class({
 	options: {},
+	fastSetOptions: false,
 	setOptions: function(){
 		if (!this.options) {
 			this.options = {};
@@ -30,11 +31,19 @@ atom.Class.Options = atom.Class({
 			// it shouldn't be link to static options
 			this.options = atom.clone(this.options);
 		}
+		var options = this.options;
 
 		for (var a = arguments, i = 0, l = a.length; i < l; i++) {
-			if (typeof a[i] == 'object') atom.extend(this.options, a[i]);
+			if (typeof a[i] == 'object') {
+				if (this.fastSetOptions) {
+					for (var k in a[i]) if (a[i].hasOwnProperty(k)) {
+						options[k] = a[i][k];
+					}
+				} else {
+					atom.extend(options, a[i]);
+				}
+			}
 		}
-		var options = this.options;
 		
 		if (this.addEvent) for (var option in options){
 			if (atom.typeOf(options[option]) == 'function' && (/^on[A-Z]/).test(option)) {
