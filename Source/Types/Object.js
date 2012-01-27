713200ea4b610f61b8bcd8c82b0d053e0c6a8ad1
TheShock <shocksilien@gmail.com>
2012-01-27 04:31:17 +0200

    Prototypes.Object. now uses Types.Object

---
+++ Source/Types/Object.js
@@ -0,0 +1,126 @@
+/*
+---
+
+name: "Types.Object"
+
+description: "Object generic methods"
+
+license:
+	- "[GNU Lesser General Public License](http://opensource.org/licenses/lgpl-license.php)"
+	- "[MIT License](http://opensource.org/licenses/mit-license.php)"
+
+requires:
+	- atom
+
+provides: Types.Object
+
+...
+*/
+
+atom.object = {
+	invert: function (object) {
+		var newObj = {};
+		for (var i in object) newObj[object[i]] = i;
+		return newObj;
+	},
+	collect: function (obj, props, Default) {
+		var newObj = {};
+		for (var i in props.toKeys()) {
+			newObj[i] = i in obj ? obj[i] : Default;
+		}
+		return newObj;
+	},
+	values: function (obj) {
+		var values = [];
+		for (var i in obj) values.push(obj[i]);
+		return values;
+	},
+	isDefined: function (obj) {
+		return typeof obj != 'undefined';
+	},
+	isReal: function (obj) {
+		return obj || obj === 0;
+	},
+	map: function (obj, fn) {
+		var mapped = {};
+		for (var i in obj) if (obj.hasOwnProperty(i)) {
+			mapped[i] = fn( obj[i], i, obj );
+		}
+		return mapped;
+	},
+	max: function (obj) {
+		var max = null, key = null;
+		for (var i in obj) if (max == null || obj[i] > max) {
+			key = i;
+			max = obj[i];
+		}
+		return key;
+	},
+	min: function (obj) {
+		var min = null, key = null;
+		for (var i in obj) if (min == null || obj[i] < min) {
+			key = i;
+			min = obj[i];
+		}
+		return key;
+	},
+	deepEquals: function (first, second) {
+		if (!first || (typeof first) !== (typeof second)) return false;
+
+		for (var i in first) {
+			var f = first[i], s = second[i];
+			if (typeof f === 'object') {
+				if (!s || !Object.deepEquals(f, s)) return false;
+			} else if (f !== s) {
+				return false;
+			}
+		}
+
+		for (i in second) if (!(i in first)) return false;
+
+		return true;
+	},
+	isEmpty: function (object) {
+		return Object.keys(object).length == 0;
+	},
+	ifEmpty: function (object, key, defaultValue) {
+		if (!(key in object)) {
+			object[key] = defaultValue;
+		}
+		return object;
+	},
+	path: {
+		parts: function (path, delimiter) {
+			return Array.isArray(path) ? path : String(path).split( delimiter || '.' );
+		},
+		get: function (object, path, delimiter) {
+			if (!path) return object;
+
+			path = Object.path.parts( path, delimiter );
+
+			for (var i = 0; i < path.length; i++) {
+				if (object != null && path[i] in object) {
+					object = object[path[i]];
+				} else {
+					return;
+				}
+			}
+
+			return object;
+		},
+		set: function (object, path, value, delimiter) {
+			path = Object.path.parts( path, delimiter );
+
+			var key = path.pop();
+
+			object = Object.path.get( object, path.length > 0 && path, delimiter );
+
+			if (object == null) {
+				return false;
+			} else {
+				object[key] = value;
+				return true;
+			}
+		}
+	}
+};
